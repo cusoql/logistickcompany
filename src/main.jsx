3918f1c82d4c5729357238d9cc0c35c2e6bc7487
cvsdxkq <119760525+cusoql@users.noreply.github.com>
2025-10-05 22:27:42 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import LogisticsSite from "./LogisticsSite";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LogisticsSite />
-  </React.StrictMode>
-);
+import React from 'react';
+export default function App(){return <h1>React App works!</h1>}
